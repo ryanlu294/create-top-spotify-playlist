f6f2be5f912146c9539cff4ad2cee2f180950337
Ryan <ryanlu294@gmail.com>
2022-11-03 17:50:35 -0700

    adds playlist function and requirements

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # create-top-spotify-playlist
 Create a 100-song playlist based on your top 10 artists.
+
+1. Install Python 3.10
+2. Create virtual environment -> python -m venv ./.venv
+3. Install pip -> python -m pip install --upgrade pip
+4. Install requirements -> pip install requirements.txt
+5. Create an app at https://developer.spotify.com/dashboard/login
+6. Set SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET variables in the .env file
+7. Enter your username and desired playlist name in the create
+8. Run run.py
---
+++ run.py
@@ -0,0 +1,38 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_top_playlist(username, playlist_name):
+    scope = ["user-top-read", "playlist-modify-public"]
+    artist_list = []
+    top_tracks = []
+    playlist_id = 0
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, username=username))
+
+    results = sp.current_user_top_artists(limit=10, time_range='medium_term')
+
+    for item in results['items']:
+        artist_list.append(item['id'])
+
+    for artist in artist_list:
+        top_ten_tracks = sp.artist_top_tracks(artist)
+        for track in top_ten_tracks['tracks']:
+            top_tracks.append(track['uri'])
+
+    sp.user_playlist_create(user=username,name=playlist_name)
+
+    playlists_object = sp.user_playlists(user=username)
+    for i, playlist in enumerate(playlists_object['items']):
+        if playlist['name'] == playlist_name:
+            playlist_id = i
+    playlist = playlists_object['items'][playlist_id]['uri']
+
+    sp.user_playlist_add_tracks(user=username,playlist_id=playlist,tracks=top_tracks)
+
+username = input('Enter username: ')
+playlist_name = input('Enter playlist name: ')
+
+create_top_playlist(username, playlist_name)
